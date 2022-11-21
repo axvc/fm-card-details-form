3cd8b116cca2f84820ecff80acfc223978968156
Artem Zverev <undemol133@gmail.com>
2022-11-21 20:31:30 +0400

    chore: config, style guide

---
+++ src/constants/Colors.ts
@@ -0,0 +1,8 @@
+export enum Colors {
+  LINEAR_GRADIENT = 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+  RED = 'hsl(0, 100%, 66%)',
+  WHITE = 'hsl(0, 0%, 100%)',
+  LIGHT_GRAYISH_VIOLET = 'hsl(270, 3%, 87%)',
+  DARK_GRAYISH_VIOLET = 'hsl(279, 6%, 55%)',
+  VERY_DARK_VIOLET = 'hsl(278, 68%, 11%)',
+}
