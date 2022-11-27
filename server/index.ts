5fbcb5b6faf41727a78d6ce81cb13eeb8b33b3ec
Artem Zverev <undemol133@gmail.com>
2022-11-27 21:58:12 +0400

    feat: add server

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const Koa = require('koa');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+
+const app = new Koa();
+
+const static_pages = new Koa();
+static_pages.use(serve(path.resolve(__dirname, '../build')));
+app.use(mount('/', static_pages));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(
+    '==> 🌎  Listening on port %s. Visit http://localhost:%s/',
+    PORT,
+    PORT,
+  );
+});
